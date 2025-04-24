ae682b83d607fd50e117c88d5e11e925250c3d88
__khulann118 <132682197+khulann11@users.noreply.github.com>
2025-04-24 14:30:05 +0800

    hh

--- scripts/script.py
+++ scripts/script.py
@@ -3,17 +3,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
  
 # Load data
-data = pd.read_csv("../data/edges.csv")
+data = pd.read_csv("data/got-s6-edges.csv")
 graph = nx.from_pandas_edgelist(data, source='Source', target='Target')
  
 # Visualize the graph
 nx.draw(graph, with_labels=True, node_color='skyblue', node_size=1500, edge_color='gray')
-plt.show()
-
-# Өгөгдөл ачаалах
-өгөгдөл = pd.read_csv("../data/edges.csv")
-график = nx.from_pandas_edgelist(өгөгдөл, эх сурвалж='Эх сурвалж', зорилтот='Зорилтот')
-
-# Графикийг дүрслэн харуулах
-nx.draw(график, шошготой=Үнэн, node_color='skyblue', node_size=1500, edge_color='саарал')
 plt.show()
